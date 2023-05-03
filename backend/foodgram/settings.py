4575a5dbf1e1bc87d8415c3449aedf58b162916c
ShivaZoid <krovvar14@yandex.ru>
2023-05-03 18:05:56 +0800

    settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -127,7 +127,7 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
